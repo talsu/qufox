b83533878dfbe7f36bd2b5a1e9f265b65c3f3762
talsu <talsu84@gmail.com>
2015-08-20 17:23:45 +0900

    Implement Qufox monitor client.

--- QufoxMonitorClient.js
+++ QufoxMonitorClient.js
@@ -5,17 +5,8 @@ var debug = require('debug')('qufox:monitor');
 
 exports.QufoxMonitorClient = (function(){
 	function QufoxMonitorClient (host, port, io) {
-		//var net = require('net');
 
-		// var client = net.connect({host:host, port:port}, function() { //'connect' listener
-		// 	console.log('client connected');
-		// 	client.write('world!\r\n');
-		// });
-
-		// client.on('error', function (err){
-		// 	console.log(err);
-		// });
-
+		var self = this;
 		debug('Start connecting monitor server - ' + host + ':' + port);
 		connect(host, port, connectCallback);
 
@@ -37,37 +28,47 @@ exports.QufoxMonitorClient = (function(){
 
 		function connectCallback(socket) {
 			debug('Connected to ' + host + ':' + port);
+			self.socket = socket;
+			self.isConnected = true;
+
 			socket.on('data', function (data) {
-				debug(data.toString());
-				//client.end();
-			});
-			socket.on('error', function(err) {
-				debug('Socket Error : ' + util.inspect(err, false, null, true));
-			});
+				debug(data.toString());				
+			});			
 			socket.on('timeout', function() {
 				debug('Socket Timed Out');
 			});
 			socket.on('close', function() {
 				debug('Socket Closed');
+				self.isConnected = false;
+				self.socket = null;
 				connect(host, port, connectCallback);
 			});			
 			socket.on('end', function() {
 				debug('Disconnected from ' + host + ':' + port);
 			});
 			
-		 	writeData(socket, {type:'sessionList', sessionList:io.sockets.adapter.rooms});		
+		 	self.sendData('sessionList', io.sockets.adapter.rooms);
 		}
+	}
 
-		function writeData(socket, obj){
-			var data = JSON.stringify(obj);
-			var success = !socket.write(data);
-			if (!success){
-				(function(socket, data){
-					socket.once('drain', function(){
-						writeData(socket, data);
-					});
-				})(socket, data);
-			}
+	QufoxMonitorClient.prototype.sendData = function (header, payload) {
+		var self = this;
+		if (self.isConnected && self.socket)
+		{
+			writeData(self.socket, JSON.stringify({header:header, payload:payload}));
+		}
+	};
+
+	function writeData(socket, data){
+		var success = socket.write(data);
+		if (!success){
+			debug("write data at once fail ... wait drain");
+			(function(socket, data){
+				socket.once('drain', function(){
+					debug("Drain");					
+					writeData(socket, data);
+				});
+			})(socket, data);
 		}
 	}
 
--- QufoxServer.js
+++ QufoxServer.js
@@ -16,38 +16,47 @@ exports.QufoxServer = (function(){
 		}
 
 		io.on('connection', function (socket){
-			debug('connected - ' + util.inspect({socketId:socket.id, client:socket.request.connection._peername}, false, null, true));
+			log('connected', {socketId:socket.id, client:socket.request.connection._peername});
+			
 			socket.emit('connected');
 			
 			socket.on('join', function (sessionId) {
-				debug('join - ' + util.inspect({socketId:socket.id, sessionId:sessionId}, false, null, true));
+				log('join', {socketId:socket.id, sessionId:sessionId});
 				socket.join(sessionId);				
 				socket.emit('joinCallback', {id:sessionId, data:'success'});
 			});
 
 			socket.on('send', function (payload) {
 				if (payload && payload.sessionId && payload.id) {
-					debug('send - ' + util.inspect({socketId:socket.id, payload:payload}, false, null, true));
+					log('send', {socketId:socket.id, payload:payload});
 					socket.broadcast.to(payload.sessionId).emit('receive', {id:payload.sessionId, data:payload.data});
 					socket.emit('sendCallback', {id:payload.id, data:'success'});
 				}
 			});
 
 			socket.on('leave', function (sessionId) {
-				debug('leave - ' + util.inspect({socketId:socket.id, sessionId:sessionId}, false, null, true));//
+				log('leave', {socketId:socket.id, sessionId:sessionId});
 				socket.leave(sessionId);
 				socket.emit('leaveCallback', {id:sessionId, data:'success'});
 			});
 
 			socket.on('disconnect', function () {
-				debug('disconnect - ' + util.inspect({socketId:socket.id}, false, null, true));
+				log('disconnect', {socketId:socket.id});
 			});
 		});
 
 		debug('Qufox server is running.');
 
 		if (monitor && monitor.host && monitor.port){
-			self.monitor = new require('./QufoxMonitorClient').QufoxMonitorClient(monitor.host, monitor.port, io);
+			var QufoxMonitorClient = require('./QufoxMonitorClient').QufoxMonitorClient;
+			self.monitor = new QufoxMonitorClient(monitor.host, monitor.port, io);
+		}
+
+		function log(header, payload) {
+			debug(header + ' - ' + util.inspect(payload, false, null, true));
+			if (self.monitor && self.monitor.isConnected) {
+				self.monitor.sendData(header, payload);
+			}
 		}
 	}
 
